9c80578e9c45e085942d8ae5e0c8440b5de1cfb3
Newsch00ler <86545418+Newsch00ler@users.noreply.github.com>
2021-11-27 20:01:52 +0800

    Создан класс частиц

--- ParticleImplementation/Form1.cs
+++ ParticleImplementation/Form1.cs
@@ -15,6 +15,7 @@ namespace ParticleImplementation
         public Form1()
         {
             InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
         }
     }
 }
---
+++ ParticleImplementation/Particle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleImplementation
+{
+    class Particle
+    {
+        public int Radius; // радуис частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+
+        public float Direction; // направление движения
+        public float Speed; // скорость перемещения
+
+        // добавили генератор случайных чисел
+        public static Random random = new Random();
+
+        // конструктор по умолчанию будет создавать кастомную частицу
+        public Particle()
+        {
+            // я не трогаю координаты X, Y потому что хочу, чтобы все частицы возникали из одного места
+            Direction = random.Next(360);
+            Speed = 1 + random.Next(10);
+            Radius = 2 + random.Next(10);
+        }
+    }
+}
